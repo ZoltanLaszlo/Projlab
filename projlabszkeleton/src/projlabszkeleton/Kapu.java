3cd86523fa98beeab62487057853514e0ce25d6d
kob07kob <diki71r@gmail.com>
2017-03-20 00:33:02 +0100

    Update Kapu.java

--- projlabszkeleton/src/projlabszkeleton/Kapu.java
+++ projlabszkeleton/src/projlabszkeleton/Kapu.java
@@ -5,11 +5,31 @@ public class Kapu implements Elem{
 	private Kapu masikkapu;
 	private int hossz;
 	private SpecialisHely helyem;
-	
+	// az alagútban lévő vonatot az alagút lépteti ha kilép visszadja a sínnek
 	@Override
 	public void lep() {
 		// TODO Auto-generated method stub
+		int k=-1;
 		
+		while(k==-1){
+			System.out.println("? kilépünk [igen][nem]");
+			//Scanner scanner = new Scanner(System.in);
+			String input = new Program().scanner.nextLine();
+			
+			if(input.equals("igen")){
+				k=0;
+				System.out.println(">>kap1{Kapu} calls"
+						+ " alagutallapot(false):void on "
+						+ "mo{Mozdony}");
+				new Mozdony().alagutAllapot(false);
+			}
+			else{
+				System.out.println(">>elso{Palya} calls"
+						+ " lep():void on "
+						+ "kap1{Kapu}");
+			}
+		}
+		//scanner.close();
 	}
 
 	@Override
@@ -17,16 +37,19 @@ public class Kapu implements Elem{
 		// TODO Auto-generated method stub
 		return null;
 	}
+	// visszadja a kapu helyét
 	public SpecialisHely hely() {
-		System.out.println("<<helyem:SpecialisHely");
+		System.out.println("<<kap2{Kapu} hely() returned s4:SpecialisHely");
 		return helyem;
 	}
 	public int alaguthossz(){
 		System.out.println("<<hossz:int");
 		return hossz;
 	}
+	
+	// visszadja a másik kaput
 	public Kapu kapu(){
-		System.out.println("<<kap:Kapu");
+		System.out.println("<<kap2{Kapu} kapu() returned kap2:Kapu");
 		return masikkapu;
 	}
 	
@@ -37,4 +60,9 @@ public class Kapu implements Elem{
 	public void ad(Kapu masikkapu){
 		System.out.println("\t{Kapu}: ad(Kapu)");
 	}
+	// belépteti a kocsit az alagútba (itt folytatjuk az alagútbejárást mivel több lépés kell
+	public void belep(Kocsi k){
+		new Program().alagutfolyt();
+	}
+
 }
