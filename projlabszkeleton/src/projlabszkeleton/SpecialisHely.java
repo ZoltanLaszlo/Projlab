e57aac98660987d372323eebdba71bdf145fb265
kob07kob <diki71r@gmail.com>
2017-03-20 00:16:27 +0100

    Update SpecialisHely.java

--- projlabszkeleton/src/projlabszkeleton/SpecialisHely.java
+++ projlabszkeleton/src/projlabszkeleton/SpecialisHely.java
@@ -47,9 +47,12 @@ public class SpecialisHely extends Sin{
 	
 		return false;
 	}
-	
+	/**
+	visszaadja van-e kapu a speciális helyen
+	**/
 	public boolean vankapu(){
-		//meg kell kérdezni a felhasználótól és azt visszaadni amit õ mond
+		System.out.println("mivel azt mondtuk alagut jon ezert itt van kapu");
+		System.out.println("<<s3{SpecialisHely} vankapu() returned true:boolean");
 		return true;
 	}
 	public Kapu kapu(){
@@ -61,7 +64,42 @@ public class SpecialisHely extends Sin{
 	}
 	
 	@Override
+	/**
+	visszaadja mi a következő elem figyelembe véve hogy a vonat jöhetett alagútból és mehet alagútba
+	**/
 	public Sin kovetkezo(Sin elozo){
+		System.out.println(">>s3{SpecialisHely} calls"
+				+ " kapu():Kapu on "
+				+ "kap1{Kapu}");
+		new Kapu().kapu();
+		System.out.println(">>s3{SpecialisHely} calls"
+				+ " hely():SpecialisHely on "
+				+ "kap2{Kapu}");
+		new Kapu().hely();
+		System.out.println(">>s3{SpecialisHely} calls"
+				+ " kovetkezo(s3):Sin on "
+				+ "s4{SpecialisHely}");
+		System.out.println("<<s4{SpecialisHely} kovetkezo(s3) returned s5:Sin");
+		System.out.println("<<s3{SpecialisHely} kovetkezo(s2) returned s5:Sin");
+		
 		return new Sin();
+	}
+	/**
+	 ha rálép egy kocsi figyelembe veszi hogy van rajta alagút és beküldi az alagútba
+	**/
+	@Override
+	public void ad(Kocsi k){
+		System.out.println(">>s3{SpecialisHely} calls"
+				+ " vankapu():boolean on "
+				+ "s3{SpecialisHely}");
+		vankapu();
+		System.out.println(">>s3{SpecialisHely} calls"
+				+ " alagutallapot(true):void on "
+				+ "mo{Mozdony}");
+		k.alagutAllapot(true);
+		System.out.println(">>s3{SpecialisHely} calls"
+				+ " belep(mo):void on "
+				+ "kap1{Kapu}");
+		new Kapu().belep(k);
 	}
 }
