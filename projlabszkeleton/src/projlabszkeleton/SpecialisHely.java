2b6f1701aed7724507e5ff850976babd08260edb
BAttila00 <bruderattila@freemail.hu>
2017-03-19 22:05:51 +0100

    Update SpecialisHely.java

--- projlabszkeleton/src/projlabszkeleton/SpecialisHely.java
+++ projlabszkeleton/src/projlabszkeleton/SpecialisHely.java
@@ -5,16 +5,32 @@ public class SpecialisHely extends Sin{
 	
 	@Override
 	public Boolean akcio(){
-		return null;
+		System.out.println("{SpecialisHely} akcio():");
+		System.out.println("\tkap1{Kapu} Created");
+		System.out.println("\ts4{SpecialisHely} calls ad(kap1: Kapu) on s4{SpecialisHely}");
+		new SpecialisHely().ad(new Kapu());
+		System.out.println("\ts4{SpecialisHely} calls ad(s4: SpecialisHely) on kap1{Kapu}");
+		new Kapu().ad(new SpecialisHely());
+		System.out.println("\tkap2{Kapu} Created");
+		System.out.println("\ts5{SpecialisHely} calls ad(kap2: Kapu) on kap1{Kapu}");
+		new Kapu().ad(new Kapu());
+		System.out.println("\ts5{SpecialisHely} calls ad(kap1: Kapu) on kap2{Kapu}");
+		new Kapu().ad(new Kapu());
+		System.out.println("s4{SpecialisHely}: akcio() returned true: boolean");
 	}
 	
 	public boolean vankapu(){
-		//meg kell kÈrdezni a felhaszn·lÛtÛl Ès azt visszaadni amit ı mond
+		//meg kell k√©rdezni a felhaszn√°l√≥t√≥l √©s azt visszaadni amit √µ mond
 		return true;
 	}
 	public Kapu kapu(){
 		return kapum;
 	}
+	
+	public void ad(Kapu kapum){
+		System.out.println("\t{SpecialisHely}: ad(Kapu)");
+	}
+	
 	@Override
 	public Sin kovetkezo(Sin elozo){
 		return new Sin();
