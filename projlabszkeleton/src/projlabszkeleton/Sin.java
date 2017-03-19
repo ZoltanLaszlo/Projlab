0b6acc107ee649d851f52048617f7ba87977bc0a
kob07kob <diki71r@gmail.com>
2017-03-20 00:26:14 +0100

    Update Sin.java

--- projlabszkeleton/src/projlabszkeleton/Sin.java
+++ projlabszkeleton/src/projlabszkeleton/Sin.java
@@ -12,8 +12,37 @@ public class Sin implements Elem{
 	public void ad(Kocsi k){
 		
 	}
-	
+	// visszadja a következő sínt az előző sín alapján
 	public Sin kovetkezo(Sin elozo){
+		String be=new String();
+		System.out.println("? Hova lépünk? [Sin][Alagut][Megallo][Valto]");
+		//Scanner scanner = new Scanner(System.in);
+		be = new Program().scanner.nextLine();
+		if(be.equals("Sin")){
+			System.out.println("<<s2{Sin} kovetkezo(s1) returned s3:Sin");
+			return new Sin();
+		}
+		if(be.equals("Alagut")){
+
+			System.out.println("<<s2{Sin} kovetkezo(s1) returned s3:SpecialisHely");
+			return new SpecialisHely();
+			
+		}
+		if(be.equals("Megallo")){
+			System.out.println("<<s2{Sin} kovetkezo(s1) returned s3:Megallo");
+			return new Megallo();
+			
+		}
+		if(be.equals("Valto")){
+			System.out.println("<<s2{Sin} kovetkezo(s1) returned s3:Valto");
+			return new Valto();
+			
+		}
+
+		//scanner.close();
+
+
+		
 		return new Sin();
 	}
 	
