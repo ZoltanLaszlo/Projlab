35008e06df71fb9b2e5b77406d440175bdeed035
“Zotya95“ <l.zotya95@gmail.com>
2017-03-19 21:35:33 +0100

    update

--- projlabszkeleton/src/projlabszkeleton/Palya.java
+++ projlabszkeleton/src/projlabszkeleton/Palya.java
@@ -4,8 +4,48 @@ public class Palya {
 	private ArrayList<Elem> elemek;
 	
 	public void start (){
-		//ez az init létrehozás után kiirjuk a képernyõre hogy létre lett hozva
+		//ez az init lï¿½trehozï¿½s utï¿½n kiirjuk a kï¿½pernyï¿½re hogy lï¿½tre lett hozva
+		Sin s1 = new Sin();
+		Megallo m = new Megallo();
+		Valto v = new Valto();
+		Mozdony mo = new Mozdony();
+		SpecialisHely s2 = new SpecialisHely();
+		Kocsi k = new Kocsi();
 		
+		s1.ad(v);
+		s1.ad(m);
+		
+		m.ad(s1);
+		m.ad(v);
+		m.ad(mo);
+		
+		mo.ad(m);
+		mo.ad(k);
+		
+		v.ad(m);
+		v.ad(s2);
+		v.ad(s1);
+		v.ad(k);
+	
+		k.ad(v);
+		k.ad(mo);
+		
+		s2.ad(v);
+		
+		mo.setUtasokSzama(0);
+		
+		k.setUtasokSzama(3);
+		
+		Mozdonytar mk = new Mozdonytar();
+		
+		mk.ad(mo);
+		
+		this.add(s1);
+		this.add(m);
+		this.add(v);
+		this.add(mo);
+		this.add(s2);
+		this.add(k);
 	}
 	public void add(Elem e){
 		
--- projlabszkeleton/src/projlabszkeleton/Program.java
+++ projlabszkeleton/src/projlabszkeleton/Program.java
@@ -4,40 +4,101 @@ import java.util.Scanner;
 
 public class Program {
 
+	private static Scanner scanner = new Scanner(System.in);
+	private static int melyseg;
+
 	public static void main(String[] args) {
-		//itt beolvassuk melyik use/case stb.
-		
-		Scanner scanner = new Scanner(System.in);
-		String input = "";
-		int menupont;
-		do {
-			System.out.print("Válassz egy működést! 1-5: ");
-			input = scanner.nextLine();
-			menupont = menupontValidacio(input, 1 ,5);
-		} while (menupont == -1);
-		
-		switch (menupont) {
-		case 1 :
-			System.out.println("inicializalas");
+		// itt beolvassuk melyik use/case stb.
+
+		melyseg = -1;
+		switch (menupontValasztas("Válassz egy működést!", 1, 5)) {
+		case 1:
+			println("inicializalas:");
+			
+			Palya palya = new Palya();
+			println("palya{Palya} Created");
+			println(">>{Program} calls start():void on palya{Palya}");
+			palya.start();
+			break;
+		case 2:
+			println("mozdonyleptetes");
 			// TODO
 			break;
-		case 2 :
-			System.out.println("mozdonyleptetes");
+		case 3:
+			println("valto allitas");
+ 			println("elso{Palya} calls akcio(): boolean on v1{Valto}");
+			new Valto().akcio();
+			break;
+		case 4:
+			println("kapu allitas");
 			// TODO
 			break;
-		case 3 :
-			System.out.println("valto allitas");
-			// TODO
-			break;
-		case 4 :
-			System.out.println("kapu allitas");
-			// TODO
-			break;
-		case 5 :
-			System.out.println("jatek vege");
+		case 5:
+			println("jatek vege");
 			// TODO
 			break;
 		}
+
+		scanner.close();
+	}
+
+	/**
+	 * Számozott menupontok választása.
+	 * 
+	 * @param menupontSzovege
+	 *            A konzolra kiírt szöveg.
+	 * @param minimum
+	 *            A legkisebb szám, amely az adott menüpontnál még értelmezett.
+	 * @param maximum
+	 *            A legnagyobb szám, amely az adott menüpontnál még értelmezett.
+	 * @return A bevitt menüpont száma
+	 */
+	public static int menupontValasztas(String menupontSzovege, int minimum, int maximum) {
+		++melyseg;
+		int menupont = -1;
+		String input = "";
+		do {
+			for (int i = 0; i < melyseg; i++)
+				System.out.print("	");
+			System.out.print(menupontSzovege + " " + minimum + "-" + maximum + ": ");
+			input = scanner.nextLine();
+			menupont = menupontValidacio(input, minimum, maximum);
+		} while (menupont == -1);
+		return menupont;
+	}
+
+	/**
+	 * Eldöntendő menüpont választása
+	 * 
+	 * @param menupontSzovege
+	 *            A konzolra kiírt kérdés.
+	 * @return True, ha igen volt a válasz, és false, ha nem
+	 */
+	public static boolean menupontValasztas(String menupontSzovege) {
+		++melyseg;
+		String input = "";
+		int valasz;
+		do {
+			for (int i = 0; i < melyseg; i++)
+				System.out.print("	");
+			System.out.print(menupontSzovege + " igen/nem: ");
+			input = scanner.nextLine();
+			valasz = menupontValidacio(input);
+		} while (valasz == -1);
+		return valasz == 1 ? true : false;
+	}
+
+	/**
+	 * Kiíratás a konzolra, az aktuális mélység figyelembevételével
+	 * 
+	 * @param str
+	 *            A kiírandó string.
+	 */
+	public static void println(String str) {
+		for (int i = 0; i < melyseg+1; i++) {
+			System.out.print("	");
+		}
+		System.out.println(str);
 	}
 
 	/**
@@ -60,7 +121,23 @@ public class Program {
 		}
 		if (menupont >= minimum && menupont <= maximum)
 			return menupont;
-		return menupont;
+		return -1;
+	}
+
+	/**
+	 * Eldöntendő kérdés validációja
+	 * 
+	 * @param input
+	 *            A konzolról beolvasott input.
+	 * @return 1, ha igen a válasz, 0, ha nem és -1 ha helytelen az input.
+	 */
+	private static int menupontValidacio(String input) {
+		input = input.toLowerCase();
+		if (input.equals("y") || input.equals("yes") || input.equals("i") || input.equals("igen"))
+			return 1;
+		if (input.equals("n") || input.equals("no") || input.equals("nem"))
+			return 0;
+		return -1;
 	}
 
 }
