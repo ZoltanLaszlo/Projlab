dc78174bd9df40020b48ae64304e6b3268211199
BAttila00 <bruderattila@freemail.hu>
2017-04-14 17:26:19 +0200

    Update Mozdonytar.java

--- projlabszkeleton/src/projlabszkeleton/Mozdonytar.java
+++ projlabszkeleton/src/projlabszkeleton/Mozdonytar.java
@@ -4,18 +4,23 @@ import java.util.ArrayList;
 public class Mozdonytar {
 	private ArrayList<Mozdony> vonatok = new ArrayList<Mozdony>();
 	
+	/**
+	 * Hozzáad egy mozdonyt a mozdonytárhoz.
+	 * @param mo A hozzáadni kívánt mozdony.
+	 */
 	public void ad (Mozdony mo){
 		vonatok.add(mo);
 	}
-	public boolean vannemuresvonat(){
-		//megkérdezzük hány vonat van és annyiszor meghívjuk egy mozdonyra az osszesures fv-t és ha valamikor false jön vissza true-t adunk adunk
-		
-		for (Mozdony mo : vonatok)
-			if (!mo.OsszesUres()) return true;
-		
-		Program.println("<<mk{Mozdonytar} calls OsszesUres():boolean on all {Mozdony}");
-		new Mozdony().OsszesUres();
-		Program.println("<<mk{MozdonyTar}: vannemuresvonat() returned false: boolean");
+	
+	/**
+	 * Megmondja, hogy van-e még nem üres vonatunk
+	 * @return true, ha még van nem üres vonat
+	 */
+	public boolean vanNemUresVonat(){
+		for(Mozdony m: vonatok){
+			if(!m.osszesUres())
+				return true;
+		}
 		return false;
 	}
 }
