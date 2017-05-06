02e714381f6b1e66c0a3c8722e5bc1d94dc14e61
kob07kob <diki71r@gmail.com>
2017-05-06 13:44:02 +0200

    Update Palya_Rajzolo.java

--- grafikus/src/projlabszkeleton/Palya_Rajzolo.java
+++ grafikus/src/projlabszkeleton/Palya_Rajzolo.java
@@ -8,12 +8,14 @@ import javax.swing.JPanel;
 public class Palya_Rajzolo extends JPanel {
 
 	/**
-	 * 
+	 * gg
 	 */
 	private static final long serialVersionUID = 1L;
 	private Sin_Graf[][] fields;
 	
-	
+	/**
+	 *Konstruktor létrehozza a főpanelt ahol vannak a grafikus elemek
+	 */
 	public Palya_Rajzolo(){
 		setSize(1000, 700);
 		setBackground(Color.green);
@@ -25,23 +27,72 @@ public class Palya_Rajzolo extends JPanel {
 			}
 		}
 	}
-	
+
+	/**
+	 * berak egy sima Sin elemet a tarolojaba
+	 * 
+	 * @param s A modelbeli Sin elem
+	 * @param x vizszintes koordináta (1-20)
+	 * @param y függöleges koordináta (1-14)
+	 * @param allapot az elem állapota (lsd. konstruktor)
+	 */
 	public void adSin(Sin s, int x, int y, String allapot){
 		fields[x-1][y-1]=new Sin_Graf(s, allapot);
 	}
+
+	/**
+	 * berak egy Valto elemet a tarolojaba
+	 * 
+	 * @param s A modelbeli Valto elem
+	 * @param x vizszintes koordináta (1-20)
+	 * @param y függöleges koordináta (1-14)
+	 * @param allapot az elem állapota (lsd. konstruktor)
+	 */
 	public void adValto(Valto s, int x, int y, String allapot){
 		fields[x-1][y-1]=new Valto_Graf(s, allapot);
 	}
+
+	/**
+	 * berak egy SpecialisHely elemet a tarolojaba
+	 * 
+	 * @param s A modelbeli SpecialisHely elem
+	 * @param x vizszintes koordináta (1-20)
+	 * @param y függöleges koordináta (1-14)
+	 * @param allapot az elem állapota (lsd. konstruktor)
+	 */
 	public void adSpecialisHely(SpecialisHely s, int x, int y, String allapot){
 		fields[x-1][y-1]=new SpecialisHely_Graf(s, allapot);
 	}
+
+	/**
+	 * berak egy Megallo elemet a tarolojaba
+	 * 
+	 * @param s A modelbeli Megallo elem
+	 * @param x vizszintes koordináta (1-20)
+	 * @param y függöleges koordináta (1-14)
+	 * @param allapot az elem állapota (lsd. konstruktor)
+	 */
 	public void adMegallo(Megallo s, int x, int y, String allapot){
 		fields[x-1][y-1]=new Megallo_Graf(s, allapot);
 	}
+
+	/**
+	 * berak egy Keresztezodes elemet a tarolojaba
+	 * 
+	 * @param s A modelbeli Keresztezodes elem
+	 * @param x vizszintes koordináta (1-20)
+	 * @param y függöleges koordináta (1-14)
+	 * @param allapot az elem állapota (lsd. konstruktor)
+	 */
 	public void adKeresztezodes(Keresztezodes s, int x, int y){
 		fields[x-1][y-1]=new Keresztezodes_Graf(s, "ker");
 	}
-	
+
+	/**
+	 * berakja a tarolójaból az összes panelt a megfelelő sorrendben
+	 * Az üres helyekre üres (háttérszínű) panelt rak be
+	 * 
+	 */
 	public void ready(){
 		for(int i=0; i<14; i++){
 			for(int t=0; t<20; t++){
